9cb90cc885c44ce48b4f3b6bd5af18e85f941dbe
SwiftLight <andrevpanchenko@gmail.com>
2022-09-13 16:13:37 +0300

    feat: add archive logic

--- notes-app/index.js
+++ notes-app/index.js
@@ -6,8 +6,13 @@ let msg = document.getElementById("msg");
 let notesList = document.querySelector(".notes-list");
 let add = document.getElementById("add");
 let taskActive = document.querySelector("#task-active");
+let taskArchived = document.querySelector("#task-archive");
 let ideaActive = document.querySelector("#idea-active");
+let ideaArchived = document.querySelector("#idea-archive");
 let randomThoughtActive = document.querySelector("#random-thought-active");
+let randomThoughtArchived = document.querySelector("#random-thought-archive");
+let archivedList = document.querySelector(".archived-list");
+let toggleNotes = document.querySelector(".toggle-notes");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -38,7 +43,8 @@ let acceptData = () => {
     text: textInput.value,
     date: dateInput.value,
     description: textarea.value,
-    category: category.value
+    category: category.value,
+    isArchived: false
   });
 
   localStorage.setItem("data", JSON.stringify(data));
@@ -50,27 +56,33 @@ let acceptData = () => {
 let createTasks = () => {
     notesList.innerHTML = "";
     data.map((x, y) => {
-        let utc = new Date().toJSON().slice(0,10);
-        let reg = /(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g;
-        if (x.description.match(reg)) {
-            console.log(x.description.match(reg));
-            x.date = x.description.match(reg);
+      let checkArc = () => {
+        if (data[y].isArchived) {
+          return "hidden";
         }
-        return (notesList.innerHTML += `
-        <li id=${y} class="list-item">
-            <span class="fw-bold">${x.text}</span>
-            <span class="small text-secondary">${utc}</span>
-            <span class="fw-bold">${x.category}</span>
-            <span class="small text-secondary">${x.description}</span>
-            <span class="small text-secondary">${x.date}</span>
+      };
+      let utc = new Date().toJSON().slice(0,10);
+      let reg = /(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g;
+      if (x.description.match(reg)) {
+        console.log(x.description.match(reg));
+        x.date = x.description.match(reg);
+      }
+      return (notesList.innerHTML += `
+        <li id=${y} class="list-item ${checkArc()}">
+          <span class="fw-bold">${x.text}</span>
+          <span class="small text-secondary">${utc}</span>
+          <span class="fw-bold">${x.category}</span>
+          <span class="small text-secondary">${x.description}</span>
+          <span class="small text-secondary">${x.date}</span>
   
-            <span class="options">
-                <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-                <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
-            </span>
+          <span class="options">
+            <i onClick="archiveTask(this)">arc</i>
+            <i onClick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+            <i onClick="deleteTask(this); createTasks()" class="fas fa-trash-alt"></i>
+          </span>
         </li>
-        `
-        );
+      `
+      );
     });
     countCategory();
     resetForm();
@@ -104,24 +116,68 @@ let resetForm = () => {
 (() => {
   data = JSON.parse(localStorage.getItem("data")) || []
   console.log(data);
+
   createTasks();
 })();
 
 function countCategory () {
     let idea = 0;
+    let ideaArc = 0;
     let task = 0;
+    let taskArc = 0;
     let randomThought = 0;
+    let randomThoughtArc = 0;
     for (let i = 0; i < data.length; i++) {
-        if (data[i].category === "Idea") {
-            idea++;
-        } else if (data[i].category === "Random Thought") {
-            randomThought++;
-        } else if (data[i].category === "Task") {
-            task++;
-        }
+      if (data[i].category === "Idea" && !data[i].isArchived) {
+        idea++;
+      } 
+      else if (data[i].category === "Idea" && data[i].isArchived) {
+        ideaArc++;
+      }
+      else if (data[i].category === "Random Thought" && !data[i].isArchived) {
+        randomThought++;
+      }  
+      else if (data[i].category === "Random Thought" && data[i].isArchived) {
+        randomThoughtArc++;
+      }
+      else if (data[i].category === "Task" && !data[i].isArchived) {
+        task++;
+      } 
+      else if (data[i].category === "Task" && data[i].isArchived) {
+        taskArc++;
+      }
     }
     taskActive.innerHTML = task;
+    taskArchived.innerHTML = taskArc;
     ideaActive.innerHTML = idea;
+    ideaArchived.innerHTML = ideaArc;
     randomThoughtActive.innerHTML = randomThought;
-    console.log(idea, task, randomThought);
+    randomThoughtArchived.innerHTML = randomThoughtArc;
+};
+
+let archiveTask = (e) => {
+  let element = e.parentElement.parentElement;
+  if (data[element.id].isArchived === true) {
+    data[element.id].isArchived = false;
+    element.classList.remove("hidden");
+  } else if (data[element.id].isArchived === false) {
+    data[element.id].isArchived = true;
+    element.classList.add("hidden");
+  }
+
+  localStorage.setItem("data", JSON.stringify(data));
+  countCategory();
+};
+
+toggleNotes.addEventListener("click", toggleNotesDisplay);
+
+function toggleNotesDisplay() {
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].isArchived);
+    if (data[i].isArchived) {
+      console.log(data[i]);
+    } else if (!data[i].isArchived) {
+      console.log(data[i]);
+    }
+  }
 };
